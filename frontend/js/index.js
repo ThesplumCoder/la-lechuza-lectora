471d3dfbbbd044fb378df775c0acd62c79913667
ThesplumCoder <michin505@hotmail.com>
2022-09-29 13:20:12 -0500

    Frontend básico terminado.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,31 @@
-let variable;
+document.addEventListener('DOMContentLoaded', ()=>{
+
+  let tablero = document.getElementById('tablero');
+
+  function cartas(){
+    let carta = document.createElement('div');
+    carta.style = 'width:25%;'
+      +'border: 1px solid black';
+    return carta;
+  }
+
+  const botonBuscar = document.getElementById('buscar');
+  botonBuscar.addEventListener('click', ()=>{
+    alert('Buscando');
+  });
+
+  const botonAnhadir = document.getElementById('anhadir');
+  botonAnhadir.addEventListener('click', ()=>{
+    tablero.appendChild(cartas());
+  });
+
+  const botonActualizar = document.getElementById('actualizar');
+  botonActualizar.addEventListener('click', ()=>{
+    alert('Actualizando');
+  });
+
+  const botonEliminar = document.getElementById('eliminar');
+  botonEliminar.addEventListener('click', ()=>{
+    alert('Eliminando');
+  });
+})
