6472d8bb633b500779e148bc5ee4abc56faddaf2
ThesplumCoder <michin505@hotmail.com>
2022-09-29 17:30:03 -0500

    Funcionalidad de creación de tarjetas completada.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,10 +2,29 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
   let tablero = document.getElementById('tablero');
 
-  function cartas(){
+  function hacerCartas(){
     let carta = document.createElement('div');
-    carta.style = 'width:25%;'
-      +'border: 1px solid black';
+    let nombre = document.createElement('p');
+    let autor = document.createElement('p');
+    let editorial = document.createElement('p');
+    let estado = document.createElement('p');
+    let pagina = document.createElement('p');
+    let nota = document.createElement('p');
+
+    carta.className = 'cartas';
+    nombre.textContent = 'La Peste';
+    autor.textContent = 'Albert Camus';
+    editorial.textContent = 'Comcosur';
+    estado.textContent = 'Leyendo';
+    pagina.textContent = '0';
+    nota.textContent = '...';
+
+    carta.appendChild(nombre);
+    carta.appendChild(autor);
+    carta.appendChild(editorial);
+    carta.appendChild(estado);
+    carta.appendChild(pagina);
+    carta.appendChild(nota);
     return carta;
   }
 
@@ -16,7 +35,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
   const botonAnhadir = document.getElementById('anhadir');
   botonAnhadir.addEventListener('click', ()=>{
-    tablero.appendChild(cartas());
+    tablero.appendChild(hacerCartas());
   });
 
   const botonActualizar = document.getElementById('actualizar');
